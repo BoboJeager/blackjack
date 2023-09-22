b8718e3d370ae208f5f8d46e0cac9fb0b9601266
bobojeager <danishhilmann98@gmail.com>
2023-09-22 21:51:32 +0800

    card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,24 +1,52 @@
 <template>
     <div @click="flipCard" class="relative w-64 h-80 transition-transform transform">
       <div v-if="flipped" class="absolute inset-0 w-full h-full bg-white backface-hidden">
-        <img class="w-64 h-56 object-cover m-0 pb-2" src="../assets/card-back.png" alt="Card Image" />
-        <h1>Card Name</h1>
-        <h2>Card Value</h2>
+        <img class="w-64 h-56 object-cover m-0 pb-2" :src="suitImage || ''" alt="Card Image" />
+        <h1>{{ cardName }}</h1>
+        <h2>{{ cardValue }}</h2>
       </div>
       <div v-else class="absolute inset-0 w-full h-full bg-black backface-hidden"></div>
     </div>
   </template>
 <script setup lang="ts">
 import { ref } from "vue";
+import { Card } from "../types/deck-types";
 
-const suitImage = ref("");
-const cardName = ref("");
-const cardValue = ref(0);
+const suitImageList = [
+                  "../assets/Card_club.svg",
+                  "../assets/Card_heart.svg", 
+                  "../assets/Card_spade.svg", 
+                  "../assets/Card_diamond.svg"
+                ]
+
+const { card } = defineProps<{
+    card: Card;
+}>();
+
+const setImage = () => {
+  switch (card.suit) {
+    case "clubs":
+      return suitImageList[0];
+    case "hearts":
+      return suitImageList[0];
+    case "spades":
+      return suitImageList[0];
+    case "diamonds":
+      return suitImageList[0];
+    default:
+      return "";
+  }
+}
+const suitImage = setImage();
+const cardName = ref(card.value);
+const cardValue = ref(card.weight);
 const flipped = ref(false);
 
+
 const flipCard = () => {
-    flipped.value = !flipped.value;
+  flipped.value = !flipped.value;
 }
+
 
 </script>
 
